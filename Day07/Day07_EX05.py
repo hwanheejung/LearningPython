2f5d0fe1df96970c9302778915ab2e66b67e0960
jhh_117 <76915945+hwanheejung@users.noreply.github.com>
2022-08-23 15:58:39 +0900

    Update Day07_EX05.py

--- Day07/Day07_EX05.py
+++ Day07/Day07_EX05.py
@@ -7,7 +7,8 @@ class Barista:
     # 생성자
     def __init__(self, name):     # 개발자가 직접 생성자를 구현
         self.name = name
-        
+    
+    # 메소드 : 클래스 내부의 함수
     def hello(self):
         print("Welcome! I'm barista {}.".format(self.name))
     def serve(self):
