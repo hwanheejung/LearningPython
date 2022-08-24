b89cb3aa2e0c97b21d78a2f9cf684e7fbfeef00c
jhh_117 <76915945+hwanheejung@users.noreply.github.com>
2022-08-25 02:20:59 +0900

    Update Day04_EX08.py

--- Day04/Day04_EX08.py
+++ Day04/Day04_EX08.py
@@ -2,24 +2,19 @@
 """
 Algorithm 2
 -----삽입정렬-----
-<idea>
-    key는 1부터 마지막 idx까지의 요소.
-    key 앞의 요소와 비교해서 key가 더 작으면 자리바꿈
-    key_idx - (1..2..3..)
 """
+
 num = [4, 3, 2, 10, 12, 1, 5, 6]
-key_idx = 1
+key = 1
 
-while key_idx < len(num):
-    print(num)
-    print("Key:{}".format(num[key_idx]))
+while key < len(num):        # key = 1, 2, 3, 4, 5, 6, 7
+    for n in range(key, 0, -1):     # 비교할 요소의 개수(n)은 key 개수와 동일하기 때문에, key부터 0까지 -1씩 줄어들며 반복
+        if num[n - 1] > num[n]:     # 앞의 요소가 뒤의 요소보다 크면, 둘이 자리바꿈
+            blank = num[n - 1]
+            num[n - 1] = num[n]
+            num[n] = blank
 
-    for i in range(key_idx):
-        print(num[key_idx - i - 1])
-        if num[key_idx] < num[key_idx - i - 1]:
-            for j in range(key_idx):
-                num[key_idx-i] = num[key_idx-j-1]
-
-
-    key_idx += 1
-
+        else:
+            break
+    key += 1
+print(num)
