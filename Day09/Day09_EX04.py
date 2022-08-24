cad1d8f7b432760d820cade26b6b26dadeb7c065
jhh_117 <76915945+hwanheejung@users.noreply.github.com>
2022-08-25 00:54:07 +0900

    Update Day09_EX04.py

--- Day09/Day09_EX04.py
+++ Day09/Day09_EX04.py
@@ -5,7 +5,8 @@ input: [3, 3, 4, 5, 6, 3, 7]  output: [2, 1, 1, 1, 1, 1]
 input: [2, 6, 6, 6, 3]  output: [1, 3, 1]
 """
 
-input = [2, 6, 6, 6, 3]
+
+input = [4, 2, 2, 2]
 output = []
 
 index = 0
@@ -16,24 +17,20 @@ while index < len(input) - 1:
         index += 1
 
     else:
-        # if input[index] == input[index + 1] and input[index] != input[index + 2]:
-        #     num += 1
-        #     output.append(num)
-        #     index += 1
-        #
-        # if input[index] == input[index + 1] and input[index] == input[index + 2]:
-        #     num += 2
-        #     output.append(num)
-        #     index += 2
         n = 1
         while n < len(input) - index - 1:
             if input[index] == input[index + n]:
                 num += 1
+            else:
+                break
+            n += 1
+        output.append(num)
+        index += num
 
-            output.append(num)
-            index += 1
-
-
+if input[-1] != input[-2]:
+    output.append(1)
+else:
+    output[-1] += 1
 
 print(output)
 
