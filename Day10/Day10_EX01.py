757a4397f210f1636c1b4627c73034a4076a7bc6
Hwanhee Jung <76915945+hwanheejung@users.noreply.github.com>
2022-08-26 00:39:44 +0900

    Update Day10_EX01.py

--- Day10/Day10_EX01.py
+++ Day10/Day10_EX01.py
@@ -1,4 +1,3 @@
-# Cont.
 """
 What is 'Heap'?
     - upgrade version of 'Tree'
@@ -13,23 +12,16 @@ What is 'Heap'?
                                                     else: break
                                            -> 없으면, break
 
-"""
+- N의 자식: 2N, 2N + 1
+- X의 부모: X // 2
 
 """
-백준 11279 : 최대 힙
-13
-input: 0 1 2 0 0 3 2 1 0 0 0 0 0    
-output: 0 2 1 3 2 1 0 0
-9
-input: 8 0 1 2 7 0 0 0 3
-output: 8 7 2 1
-"""
-
-class Heap:
+class Heap():
     def __init__(self):
+        self.cursor = 1
         self.size = 13
         self.heap = [None] * self.size
-        self.cursor = 1
+
 
     def isFull(self):
         if self.cursor == self.size:
@@ -43,31 +35,63 @@ class Heap:
         else:
             return False
 
-    def parent(self, child):
-        if child == 1:
-            return False
-        elif child // 2 >= 1:
-            return self.heap[self.cursor // 2]
-        else:
-            return False
-
-    def children(self, parent):
-        if parent * 2 + 1 <= self.size:
-            self.chd1 = self.heap[parent * 2]
-            self.chd2 = self.heap[parent * 2 + 1]
-            return self.chd1, self.chd2
-
-
     def add(self, item):
         if not self.isFull():
             self.heap[self.cursor] = item
-            while self.parent():
-                if
-            """
-            2) while 부모 노드 있는지 확인 -> 있으면, if 부모 노드 < 새 노드 : 비교해서 자리 바꿈
-                                             else: break
-                                          -> 없으면, break"""
+            self.cursor += 1
+
+            for c in range(self.cursor - 1, 1, -1):
+                if c // 2 >= 1 and self.heap[c//2] < self.heap[c]:      # if 부모가 있고 and 부모<자식:
+                    self.heap[c//2], self.heap[c] = self.heap[c], self.heap[c//2]   # change
+                else:
+                    continue
 
     def delete(self):
+        if not self.isEmpty():
+            # root node와 끝노드 change
+            if self.cursor == 2:
+                del self.heap[1]
+            else:
+                self.heap[1], self.heap[self.cursor - 1] = self.heap[self.cursor - 1], self.heap[1]
+                del self.heap[self.cursor - 1]
+            self.cursor -= 1
 
-h = Heap()
+            # 정렬
+            for c in range(1, self.cursor - 1, 1):
+                # 자식이 둘: 1) 부모 < 자식1 or 자식 2 -> 큰자식과 바꿈  2) 부모 > 자식 둘 : cont.
+                if self.heap[c * 2] is not None and self.heap[(c * 2) + 1] is not None:
+                    if self.heap[c] < self.heap[c * 2] or self.heap[c] < self.heap[c * 2 + 1]:
+                        if self.heap[c * 2] > self.heap[(c * 2) + 1]:
+                            self.heap[c], self.heap[c * 2] = self.heap[c * 2], self.heap[c]
+                        else:
+                            self.heap[c], self.heap[(c * 2) + 1] = self.heap[(c * 2) + 1], self.heap[c]
+                    else:
+                        continue
+                # 자식이 하나
+                elif self.heap[c * 2] is not None and self.heap[(c * 2) + 1] is None:
+                    if self.heap[c * 2] > self.heap[c]:
+                        self.heap[c], self.heap[c * 2] = self.heap[c * 2], self.heap[c]
+                    else:
+                        continue
+                # 자식이 없어
+                else:
+                    continue
+
+
+
+
+
+heap = Heap()
+heap.add(5) 
+heap.add(1)
+heap.add(7)
+heap.add(8)
+heap.add(2)
+heap.add(10)
+heap.add(11)
+heap.delete()
+heap.delete()
+
+
+print(heap.heap)
+
