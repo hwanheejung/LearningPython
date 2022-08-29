712afae59e6008db65ecec6da1a9b50f7ecb54c5
Hwanhee Jung <76915945+hwanheejung@users.noreply.github.com>
2022-08-30 01:31:38 +0900

    Update Day04_EX06.py

--- Day04/Day04_EX06.py
+++ Day04/Day04_EX06.py
@@ -6,7 +6,7 @@ Algorithm 1
 num = [9, 1, 7, 2, 3, 8, 10, 0, 4]
 
 idx = 0
-while idx < len(num) - 2:
+while idx <= len(num) - 2:
     for i in range(len(num)-1):
         if num[i] > num[i+1]:
             blank = num[i]
